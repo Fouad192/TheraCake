5dcea8a6c91d2881d74473a54e769abcf948235b
Mohamed Ayman <57352863+MohamedAyman74@users.noreply.github.com>
2022-11-30 21:48:59 +0200

    search

--- components/adminOrders.js
+++ components/adminOrders.js
@@ -8,63 +8,70 @@ import { useSession } from "next-auth/react";
 import OrderItem from "./orderItem";
 import OrderCheckout from "../models/order";
 function AdminOrders(props) {
-  let [orderDetails, toggleOrderDetails] = useState(false);
-  let [statusPopup, openStatusPopup] = useState(false);
-  let [search, setSearch] = useState();
-  let [result, setResult] = useState();
-  let [isLoading, setLoading] = useState(false);
-  function searchByName(e) {
-    setSearch(e.target.value);
-  }
-  // useEffect(() => {
-  // let bla = props.orders.filter(
-  //   (order) => order.firstName === search || order.lastName === search
-  // );
+    const [orderDetails, toggleOrderDetails] = useState(false);
+    const [statusPopup, openStatusPopup] = useState(false);
+    const [search, setSearch] = useState("");
+    const [result, setResult] = useState([]);
 
-  // if (bla.constructor === Array) {
-  //   setResult((result = bla));
-  // }
-
-  // if (!result) {
-  //   setLoading(false);
-  // } else if (result === bla) {
-  //   setLoading(true);
-  // }
-  // }, [result])
-
-  let toggleStatusPopup = () => {
-    if (!statusPopup) {
-      openStatusPopup(true);
-    } else {
-      openStatusPopup(false);
+    function searchByName(e) {
+        setSearch(e.target.value);
     }
-  };
-  return (
-    <>
-      <section className={classes.adminOrderGrid}>
-        <div className={classes.adminOrderInputs}>
-          <input
-            type="search"
-            value={search}
-            onChange={(e) => searchByName(e)}
-          />
-          <input type="sort" />
-        </div>
+    useEffect(() => {
+        const searchResult = props.orders.filter((order) =>
+            `${order.firstName.toLowerCase()} ${order.lastName.toLowerCase()}`.includes(
+                search.toLowerCase()
+            )
+        );
 
-        <div className={classes.filter}>
-          <button>All</button>
-          <button>Pending</button>
-          <button>Accepted</button>
-          <button>Ready</button>
-          <button>Dispatched</button>
-          <button>Completed</button>
-        </div>
-        {isLoading
-          ? result.map((r, index) => <OrderItem order={r} />)
-          : props.orders.map((order, index) => <OrderItem order={order} />)}
-      </section>
-    </>
-  );
+        if (!!searchResult.length) {
+            setResult(searchResult);
+        }else{
+          setResult([])
+        }
+
+        if (!search.length) {
+            setResult([]);
+        }
+        console.log(result);
+    }, [search]);
+
+    // let toggleStatusPopup = () => {
+    //     if (!statusPopup) {
+    //         openStatusPopup(true);
+    //     } else {
+    //         openStatusPopup(false);
+    //     }
+    // };
+    return (
+        <>
+            <section className={classes.adminOrderGrid}>
+                <div className={classes.adminOrderInputs}>
+                    <input
+                        type="search"
+                        value={search}
+                        onChange={(e) => setSearch(e.target.value)}
+                    />
+                    <input type="sort" />
+                </div>
+
+                <div className={classes.filter}>
+                    <button>All</button>
+                    <button>Pending</button>
+                    <button>Accepted</button>
+                    <button>Ready</button>
+                    <button>Dispatched</button>
+                    <button>Completed</button>
+                </div>
+                {result?.length &&
+                    result.map((r, index) => <OrderItem order={r} />)}
+                {!result.length &&
+                    !search &&
+                    props.orders.map((order, index) => (
+                        <OrderItem order={order} />
+                    ))}
+            </section>
+        </>
+    );
 }
 
 export default AdminOrders;
