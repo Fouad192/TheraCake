03c24746457cdd0b736174b37d7d58e38900b126
Fouad192 <anwarcitcm@gmail.com>
2022-12-27 14:00:27 +0200

    Update Menu.js

--- components/Menu.js
+++ components/Menu.js
@@ -116,7 +116,7 @@ function Menu(props) {
           <div className={classes.offer}>
             <p>Buy ONE get the 2nd 20% off</p>
             <p>Buy TWO get the 3rd 30% off</p>
-            <p>FREE shipping for orders more than 450LE</p>
+            <p>FREE Shipping on orders over 450EGP</p>
           </div>
           {isAuthorized ? (
             <button className={classes.addItemBtn} onClick={toggleAddItemBtn}>
