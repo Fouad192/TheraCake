4be9e910a321a053c436af0d9c0b61d5476a4417
Fouad192 <anwarcitcm@gmail.com>
2023-01-02 17:35:41 +0200

    Update Menu.js

--- components/Menu.js
+++ components/Menu.js
@@ -113,11 +113,10 @@ function Menu(props) {
               Brownies
             </button>
           </div>
-          {/* <div className={classes.offer}>
-            <p>Buy ONE get the 2nd 20% off</p>
-            <p>Buy TWO get the 3rd 30% off</p>
-            <p>FREE Shipping on orders over 450EGP</p>
-          </div> */}
+          <div className={classes.offer}>
+            <p>We are sorry for not accepting orders currently due to maintainance, thank you for understanding</p>
+         
+          </div>
           {isAuthorized ? (
             <button className={classes.addItemBtn} onClick={toggleAddItemBtn}>
               Add Item
@@ -265,7 +264,7 @@ function Menu(props) {
             ))}
           </div>
         </div>
-        {itemProps ? <SelectionPanel selectionData={itemProps} /> : null}
+        {/* {itemProps ? <SelectionPanel selectionData={itemProps} /> : null} */}
 
         {/* {returnSelectionPanel()} */}
       </section>
