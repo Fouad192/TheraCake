758bf6817b53b9ccf06ed7af7163744985dcc302
Fouad192 <anwarcitcm@gmail.com>
2023-01-26 21:44:16 +0200

    Update checkout.js

--- pages/api/checkout.js
+++ pages/api/checkout.js
@@ -9,7 +9,7 @@ export default async function handler(req, res) {
       await checkoutdetails.save();
       res.status(201).json({ message: "Checked out!" });
     } catch (err) {
-      console.log(err.message);
+      alert(err.message);
     }
   } else if(req.method === 'PUT') {
    await dbConnect();
