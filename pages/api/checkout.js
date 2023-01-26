3503b642d9d59b4f8c49e3fa0a52dda5f16dbbbd
Fouad192 <anwarcitcm@gmail.com>
2023-01-26 20:28:51 +0200

    Revert "Update checkout.js"
    This reverts commit d86297f007c7a9277ae10f7a307943fa9e1eb9cb.

--- pages/api/checkout.js
+++ pages/api/checkout.js
@@ -4,41 +4,33 @@ export default async function handler(req, res) {
   if (req.method === "POST") {
     await dbConnect();
     try {
-      if (Object.values(req.body).indexOf(undefined) > -1) {
-        alert("Please enter your address data correctly");
-      } else {
-        const orderCheckoutDetails = req.body;
-        const checkoutdetails = new OrderCheckout(orderCheckoutDetails);
-        await checkoutdetails.save();
-        res.status(201).json({ message: "Checked out!" });
-      }
+      const orderCheckoutDetails = req.body;
+      const checkoutdetails = new OrderCheckout(orderCheckoutDetails);
+      await checkoutdetails.save();
+      res.status(201).json({ message: "Checked out!" });
     } catch (err) {
       console.log(err.message);
     }
-  } else if (req.method === "PUT") {
-    await dbConnect();
-    const toBeSet = JSON.parse(req.body);
-    OrderCheckout.findOneAndUpdate(
-      { _id: toBeSet[1] },
-      { status: toBeSet[0] },
-      { new: true },
-      (err, doc) => {
-        if (err) {
-          console.log(err);
+  } else if(req.method === 'PUT') {
+   await dbConnect();
+   const toBeSet = JSON.parse(req.body);
+      OrderCheckout.findOneAndUpdate({_id: toBeSet[1]}, {status: toBeSet[0]}, {new: true}, (err, doc) => {
+        if(err) {
+          console.log(err)
         }
-        console.log(doc);
-      }
-    );
-    res.status(200).json({ message: toBeSet[0] });
-  } else if (req.method === "DELETE") {
-    await dbConnect();
-    try {
-      const id = req.body;
+        console.log(doc)
+      })
+      res.status(200).json({message: toBeSet[0]})
+    
+  } else if(req.method === 'DELETE') {
+     await dbConnect();
+     try {
+       const id = req.body;
 
-      await OrderCheckout.findByIdAndDelete(id);
-      res.status(200).json({ message: `${id} order deleted` });
-    } catch (err) {
-      console.log(err);
-    }
+       await OrderCheckout.findByIdAndDelete(id);
+       res.status(200).json({ message: `${id} order deleted` });
+     } catch (err) {
+       console.log(err);
+     }
   }
 }
