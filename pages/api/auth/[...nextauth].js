a7467fac2a09464dedd1fcfbb329e554aa601c53
Fouad192 <anwarcitcm@gmail.com>
2023-02-11 02:39:50 +0200

    Revert "Update [...nextauth].js"
    This reverts commit 30538e59ad5773aad9469dbe93a2e3c46f0b21a9.

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,26 +3,19 @@ import GoogleProvider from "next-auth/providers/google";
 import FacebookProvider from "next-auth/providers/facebook";
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 import clientPromise from "../../../lib/mongodb";
-import Google from "next-auth/providers/google";
-
 export default NextAuth({
   providers: [
-    // Google
-    Google({
+    GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      // authorization: {
-      //   params: {
-      //     redirect_uri: "https://theracakecairo.com/api/auth/callback/google",
-      //     prompt: "consent",
-      //     scope: "email",
-      //   },
-      // },
+      authorization:
+        "https://accounts.google.com/o/oauth2/auth?response_type=code&prompt=consent",
     }),
     FacebookProvider({
       clientId: process.env.FACEBOOK_CLIENT_ID,
       clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
       authorization: {
+        
         params: {
           redirect_uri: "https://theracakecairo.com/api/auth/callback/facebook",
           prompt: "consent",
@@ -33,8 +26,8 @@ export default NextAuth({
       },
     }),
   ],
-  debug: true,
-  secret: process.env.NEXTAUTH_SECRET,
+  // debug: true,
+  secret: process.env.JWT_SECRET,
   adapter: MongoDBAdapter(clientPromise),
   callbacks: {
     async session({ session, token, user }) {
