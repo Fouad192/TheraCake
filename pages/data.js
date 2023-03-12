488f0d5b399b5ef629b46b23dbad1e8354e9b615
Fouad192 <anwarcitcm@gmail.com>
2023-03-12 18:27:20 +0200

    Sort & Search

--- pages/data.js
+++ pages/data.js
@@ -11,6 +11,48 @@ function Data({ items }) {
   // }
   const [allNumbers, setAllNumbers] = useState([]);
   const [dataArray, setDataArray] = useState([]);
+  const [result, setResult] = useState([]);
+  const [sort, setSort] = useState("");
+  const [filter, setFilter] = useState([]);
+  const [search, setSearch] = useState("");
+
+
+  useEffect(() => {
+    const searchResult = filter.filter((client) =>
+      `${client[0]?.firstName?.toLowerCase()} ${client[0]?.lastName?.toLowerCase()}`.includes(
+        search.toLowerCase()
+      )
+    );
+
+    if (!!searchResult.length) {
+      setFilter(searchResult);
+    } else {
+      setFilter([]);
+    }
+
+    if (!search.length) {
+      setFilter(dataArray);
+    }
+  }, [search]);
+
+  useEffect(() => {
+    const high = [...filter].sort(
+      (a, b) => parseFloat(b.length) - parseFloat(a.length)
+    );
+
+    const low = [...filter].sort(
+      (a, b) => parseFloat(a.length) - parseFloat(b.length)
+    );
+
+    if (sort === "high") {
+      setFilter(high);
+    } else if (sort === "low") {
+      setFilter(low);
+    } else if (sort === "default") {
+      setFilter(dataArray);
+    }
+  }, [sort, filter]);
+
   useEffect(() => {
     items.map((item) => {
       let stringNumber = item.mobile?.toString();
@@ -28,27 +70,36 @@ function Data({ items }) {
       arr.push(foundDocument);
     }
     setDataArray(arr);
+    setFilter(arr)
   }, [items]);
   return (
     <>
       <Navbar />
       <section className={classes.adminOrderGrid}>
         <div className={classes.adminOrderInputs}>
-          <input type="search" placeholder="Search by name" />
-          <select defaultValue="" id={classes.sortInput}>
+          <input
+            type="search"
+            placeholder="Search by name"
+            onChange={(e) => setSearch(e.target.value)}
+            value={search}
+          />
+          <select
+            defaultValue=""
+            id={classes.sortInput}
+            onChange={(e) => setSort(e.target.value)}
+          >
             <option value="default">Default Sorting</option>
             <option value="high">Price High to Low</option>
             <option value="low">Price Low to High</option>
-            <option value="new">Recent Orders</option>
-            <option value="old">Oldest Orders</option>
           </select>
         </div>
 
-        {dataArray.map((client, index) => (
-          <ClientData client={client} key={index} />
-        ))}
+        {filter &&
+          filter.map((client, index) => (
+            <ClientData client={client} key={index} />
+          ))}
       </section>
-      <Footer/>
+      <Footer />
     </>
   );
 }
