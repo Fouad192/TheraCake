d418e2f0ce0b9297b28524ae00e20b7ae8a6a390
Fouad192 <anwarcitcm@gmail.com>
2023-05-20 17:20:49 +0300

    Fix map

--- components/orderItem.js
+++ components/orderItem.js
@@ -222,12 +222,12 @@ function OrderItem({ order }) {
                 </div>
                 <div className={classes.toppingsDiv}>
                   <div>
-                    {item.specialBites.map((specialObj) => {
+                    {item?.specialBites?.map((specialObj) => {
                       return <p key={specialObj.name}>{specialObj.name}</p>;
                     })}
                   </div>
                   <div>
-                    {item.specialBites.map((specialObj) => {
+                    {item?.specialBites?.map((specialObj) => {
                       return <p key={specialObj.name}>{specialObj.price}</p>;
                     })}
                   </div>
